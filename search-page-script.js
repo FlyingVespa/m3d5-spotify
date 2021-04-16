2ff494b29a9243b72a362f6db90581e99c687042
MSajedian <mohammadsajedian@gmail.com>
2021-04-16 15:11:58 +0100

    update links

--- search-page-script.js
+++ search-page-script.js
@@ -1,10 +1,10 @@
 const searchButtonFun = () => {
-    const divRow = document.querySelector(".albums");
-    divRow.innerHTML=""
-    const searchInput = document.getElementById("search-input").value
-    console.log('searchInput:', searchInput)
-    console.log(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchInput}`);
-    const myfetch = fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchInput}`)
+  const divRow = document.querySelector(".albums");
+  divRow.innerHTML = ""
+  const searchInput = document.getElementById("search-input").value
+  console.log('searchInput:', searchInput)
+  console.log(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchInput}`);
+  const myfetch = fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchInput}`)
     .then((res) => res.json())
     .then((data) => loadAlbums(data.data))
     .catch((error) => alert(error));
@@ -12,39 +12,42 @@ const searchButtonFun = () => {
 
 
 const loadAlbums = (albums) => {
-    // console.log('albums:', albums)
-    const divRow = document.querySelector(".albums");
-    albums.forEach((element) => {
-        divRow.innerHTML += `
+  const divRow = document.querySelector(".albums");
+  albums.forEach((element) => {
+    divRow.innerHTML += `
         
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
           <div class="card mb-4">
-          <a href="https://striveschool-api.herokuapp.com/api/deezer/album/${element.album.id}">
+          <a href="${location.origin}/albumpage.html?id=${element.album.id}" class="text-center">
             <img
               src="${element.album.cover}"
-              alt=""
+              alt="${element.album.id}"
             />
             </a>
             <div class="card-body">
               <p class="card-text text-center">
               ${element.album.title}
               </p>
-              <p class="card-text text-center card-artist"><a href="https://striveschool-api.herokuapp.com/api/deezer/artist/${element.artist.id}">${element.artist.name}</a></p>
+              <p class="card-text text-center card-artist"><a href="${location.origin}/artist.html?id=${element.artist.id}">${element.artist.name}</a></p>
             </div>
           </div>
         </div>
         
       `;
-    });
+  });
 }
 
-window.onload = () =>{
-    // console.log('location.href:', location.href)
-    document.getElementById('search-input').addEventListener('keypress', function (e) {
-        if (e.key === 'Enter') {
-            searchButtonFun()
-        }
-    });
+window.onload = () => {
+  // console.log('albums:', albums)
+  // console.log('location:', location)
+  // console.log(`${location.origin}${location.pathname}`)
+  console.log(`${location.origin}`)
+  // console.log('location.href:', location.href)
+  document.getElementById('search-input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      searchButtonFun()
+    }
+  });
 }
 
 
