fd4e569243e1261f2d7c8624562a97981b447633
MSajedian <mohammadsajedian@gmail.com>
2021-04-16 14:37:40 +0100

    Update albums

--- search-page-script.js
+++ search-page-script.js
@@ -1,4 +1,6 @@
 const searchButtonFun = () => {
+    const divRow = document.querySelector(".albums");
+    divRow.innerHTML=""
     const searchInput = document.getElementById("search-input").value
     console.log('searchInput:', searchInput)
     console.log(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchInput}`);
@@ -8,22 +10,60 @@ const searchButtonFun = () => {
     .catch((error) => alert(error));
 }
 
+
 const loadAlbums = (albums) => {
-    console.log('albums:', albums)
+    // console.log('albums:', albums)
     const divRow = document.querySelector(".albums");
     albums.forEach((element) => {
         divRow.innerHTML += `
-        <div class="song row mb-5">
-          <img src="${element.album.cover}" class="song-image mr-3 img-fluid ml-2" alt="..." />
-        <div class="song-title col-8 text-left">
-        ${element.album.title}
-          <div class="song-artists"><a href="">${element.artist.name}</a></div>
+        
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <div class="card mb-4">
+          <a href="https://striveschool-api.herokuapp.com/api/deezer/album/${element.album.id}">
+            <img
+              src="${element.album.cover}"
+              alt=""
+            />
+            </a>
+            <div class="card-body">
+              <p class="card-text text-center">
+              ${element.album.title}
+              </p>
+              <p class="card-text text-center card-artist"><a href="https://striveschool-api.herokuapp.com/api/deezer/artist/${element.artist.id}">${element.artist.name}</a></p>
+            </div>
+          </div>
         </div>
-        <div class="song-length col-1">${element.duration}:00</div>
-      </div>
-        `;
+        
+      `;
     });
 }
+
+window.onload = () =>{
+    // console.log('location.href:', location.href)
+    document.getElementById('search-input').addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            searchButtonFun()
+        }
+    });
+}
+
+
+// const loadSongs = (songs) => {
+//     console.log('songs:', songs)
+//     const divRow = document.querySelector(".songs");
+//     songs.forEach((element) => {
+//         divRow.innerHTML += `
+//         <div class="song row mb-5">
+//           <img src="${element.album.cover}" class="song-image mr-3 img-fluid ml-2" alt="..." />
+//         <div class="song-title col-8 text-left">
+//         ${element.album.title}
+//           <div class="song-artists"><a href="">${element.artist.name}</a></div>
+//         </div>
+//         <div class="song-length col-1">${element.duration}:00</div>
+//       </div>
+//         `;
+//     });
+// }
 
 
 // if ("URLSearchParams" in window) {
